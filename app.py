42b5b2a0cb4982ad8a48d5e5688e21d7e2b778b9
Ahmedi Fatima <79292645+alinashifa@users.noreply.github.com>
2022-11-30 19:34:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,16 @@ def main():
   st.title("Addition")
   html_temp = """
   <div style="background-color:tomato;padding:10px">
-  <h2 style="color:white;text-align:center;">Addition of 2 numbers using Streamlit</h2>
+  <h2 style="color:white;text-align:center;">Addition of 2 numbers</h2>
   </div>
   """
   st.markdown(html_temp,unsafe_allow_html=True)
-  num1 = st.number_input("Number 1")
-  num2 = st.number_input("Number 2")
+  number1 = st.number_input("Number 1")
+  number2 = st.number_input("Number 2")
   result = num1 + num2
   st.success('The output is {}'.format(result))
   if st.button("Made By"):
-      st.text("Atharva Ramgirkar")
-      st.text("22ds1000188")
+      st.text("Ahmedi Fatima")
 
 if __name__=='__main__':
   main()
