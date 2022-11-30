a7018e34bca5304317d9a9f8eaf7f91a8e9459c1
Ahmedi Fatima <79292645+alinashifa@users.noreply.github.com>
2022-11-30 19:37:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ import streamlit as st
 
 def main():
   st.title("Addition")
-  html_temp = """
-  <div style="background-color:pink;padding:10px">
-  <h2>Addition of 2 numbers</h2>
-  </div>
-  """
   st.markdown(html_temp,unsafe_allow_html=True)
   number1 = st.number_input("Number 1")
   number2 = st.number_input("Number 2")
