035fc45a3a24b5542eed1ba8b283e8dc3da9fa8b
Ahmedi Fatima <79292645+alinashifa@users.noreply.github.com>
2022-11-30 19:34:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def main():
   st.markdown(html_temp,unsafe_allow_html=True)
   number1 = st.number_input("Number 1")
   number2 = st.number_input("Number 2")
-  result = num1 + num2
+  result = number1 + number2
   st.success('The output is {}'.format(result))
   if st.button("Made By"):
       st.text("Ahmedi Fatima")
