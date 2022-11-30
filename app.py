ec901eb986aa4194e64c51af480fdf1a8613f818
Ahmedi Fatima <79292645+alinashifa@users.noreply.github.com>
2022-11-30 19:38:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ import streamlit as st
 
 def main():
   st.title("Addition")
-  st.markdown(html_temp,unsafe_allow_html=True)
   number1 = st.number_input("Number 1")
   number2 = st.number_input("Number 2")
-  result = number1 + number2
-  st.success('The output is {}'.format(result))
+  sum = number1 + number2
+  st.success('The output is {}'.format(sum))
   if st.button("Made By"):
       st.text("Syeda Ahmedi Fatima")
 
